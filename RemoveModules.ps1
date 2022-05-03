1a104e1ea42e49f63ec0f7231c97fe2ad800aaa0
Gary Clausen <grxclausen@gmail.com>
2022-05-03 15:56:01 -0400

    Added new PS Script RemoveModules.ps1

---
+++ RemoveModules.ps1
@@ -0,0 +1,12 @@
+﻿$installedModules = Get-InstalledModule
+
+foreach ($module in $installedModules) {
+
+    $moduleName = $module.Name
+
+    if ( $moduleName -like "OCI*" ) {
+        Write-Host($moduleName)
+
+        Uninstall-Module -Name $moduleName
+    }
+}
